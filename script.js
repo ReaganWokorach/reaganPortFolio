714567f5760cb46cf6e782fd8f04c0078dbac5ef
Reagan Wokorach <106845223+ReaganWokorach@users.noreply.github.com>
2026-05-05 12:15:13 +0300

    Updated all again

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('nav-menu');
 
-    hamburger.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-        // Changes bars to X when open
+        const hamburger = document.getElementById('hamburger');
+        const navMenu = document.getElementById('nav-menu');
         const icon = hamburger.querySelector('i');
-        icon.classList.toggle('fa-bars');
-        icon.classList.toggle('fa-times');
-    });
 
-    // Automatically close menu when a link is clicked
-    document.querySelectorAll('.nav-links a').forEach(link => {
-        link.addEventListener('click', () => {
-            navMenu.classList.remove('active');
-            hamburger.querySelector('i').classList.replace('fa-times', 'fa-bars');
+        hamburger.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+            // Toggle between Hamburger and X icon
+            icon.classList.toggle('fa-bars');
+            icon.classList.toggle('fa-times');
         });
-    });
+
+        function closeMenu() {
+            navMenu.classList.remove('active');
+            icon.classList.add('fa-bars');
+            icon.classList.remove('fa-times');
+        }
