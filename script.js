c7ab0a898964c69cdfa9a0cf0570926190080b4c
Reagan Wokorach <106845223+ReaganWokorach@users.noreply.github.com>
2026-05-05 09:13:21 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
-const navLinks = document.querySelectorAll('.nav-links a');
-
-// Open/Close Menu
-hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-    // Change icon from Bars to X
-    const icon = hamburger.querySelector('i');
-    icon.classList.toggle('fa-bars');
-    icon.classList.toggle('fa-times');
-});
-
-// Close menu when a link is clicked
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navMenu.classList.remove('active');
+        const hamburger = document.getElementById('hamburger');
+        const navMenu = document.getElementById('nav-menu');
         const icon = hamburger.querySelector('i');
-        icon.classList.add('fa-bars');
-        icon.classList.remove('fa-times');
-    });
-});
+
+        hamburger.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+            // Toggle between Hamburger and X icon
+            icon.classList.toggle('fa-bars');
+            icon.classList.toggle('fa-times');
+        });
+
+        function closeMenu() {
+            navMenu.classList.remove('active');
+            icon.classList.add('fa-bars');
+            icon.classList.remove('fa-times');
+        }
